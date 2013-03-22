5031f0f990098dd9b2015e3a5cdfa52f637bdd2d
manfredzab <manfredas@zabarauskas.com>
2013-03-22 09:08:00 +0000

    Added comments.

--- include/arguments.h
+++ include/arguments.h
@@ -13,9 +13,10 @@ namespace uk_ac_ox_cs_c875114
 struct Arguments
 {
     JoinAlgorithmType join_algorithm_type; /**< Join algorithm type (@see JoinAlgorithmType). */
-    std::string       query_file;
-    std::string       database_file;
-    bool              output_time;
+    std::string       query_file;          /**< Query file name. */
+    std::string       database_file;       /**< Database file name. */
+    bool              output_time;         /**< Flag indicating whether the time or the joined relation tuples */
+                                           /**  should be printed to screen when the program terminates. */
 };
 
 } /* namespace uk_ac_ox_cs_c875114 */
